2e4ab63352f80b62da76cf101bdaf94f423f65d9
HaYeon-Jang <73008049+HaYeon-Jang@users.noreply.github.com>
2020-12-09 00:21:01 +0900

    Update script.js

--- script.js
+++ script.js
@@ -49,6 +49,22 @@ function renderPage(date) {
     id_date.innerText = date.toLocaleString();
 }
 
+function counter(){
+    var dday = new Date("Dec 12,2019,09:00:00").getTime(); //디데이
+	setInterval(function(){
+		var now = new Date(); //현재 날짜 가져오기
+		var distance = dday - now;
+		var d = Math.floor(distance / (1000 * 60 * 60 * 24));
+		var h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		var m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		var s = Math.floor((distance % (1000 * 60)) / 1000);
+		if(s < 10){
+			s = '0'+s;
+		}
+		$('.content').html(d+':'+h+':'+m+':'+s)
+	}, 1000);
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     var date = new Date();
     renderPage(date);
